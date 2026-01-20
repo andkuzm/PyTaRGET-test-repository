9e1c82b74261c1f0f410201e3c7c3e0123e32432
kandr <kandrei704@gmail.com>
2026-01-20 22:09:55 +0200

    good test commit

--- tests/test_source.py
+++ tests/test_source.py
@@ -2,6 +2,5 @@ from src.source import get_status_code
 
 
 def test_get_status_code():
-    # outdated expectation: assumes redirect
     code = get_status_code("https://httpbin.org/status/200")
-    assert code == 302
+    assert code == 200
