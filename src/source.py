6d4fa56a5955e9a217a28a841aefbb4b3d8f75f9
kandr <kandrei704@gmail.com>
2026-01-21 20:55:38 +0200

    good commit, changes to source and test

--- src/source.py
+++ src/source.py
@@ -2,7 +2,7 @@ import requests
 
 def get_status_code(url):
     """
-    Return HTTP status code for the given URL.
+    Return HTTP status code and URL.
     """
     response = requests.get(url)
-    return response.status_code
+    return response.status_code, response.url
--- tests/test_source.py
+++ tests/test_source.py
@@ -2,6 +2,6 @@ from src.source import get_status_code
 
 
 def test_get_status_code():
-    # unnecessary comment
-    code = get_status_code("https://httpbin.org/status/200")
+    code, url = get_status_code("https://httpbin.org/status/200")
     assert code == 200
+    assert url.startswith("https://")
